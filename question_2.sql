a0a9c196f1fdde191f4acebf7afc5f4b34168a77
Deri <vojtechderner@seznam.cz>
2025-01-29 18:51:27 +0100

    Union delete, where changed

--- question_2.sql
+++ question_2.sql
@@ -2,24 +2,14 @@
 -- a poslední srovnatelné období v dostupných datech cen a mezd?
 
 SELECT 
-foodstuff,
-salary_year,
-round(avg(avg_price),2) price,
-round(avg(avg_salary),2) salary,
-FLOOR(avg(avg_salary)/avg(avg_price)) ratio 
+	foodstuff,
+	salary_year,
+	round(avg(avg_price),2) price,
+	round(avg(avg_salary),2) salary,
+	FLOOR(avg(avg_salary)/avg(avg_price)) ratio 
 FROM t_vojtech_derner_project_sql_primary_final AS tvdpspf 
-WHERE salary_year IN (2006,2018)
-AND foodstuff LIKE 'Mléko polotučné pasterované'
-GROUP BY foodstuff, salary_year
-UNION 
-SELECT 
-foodstuff,
-salary_year,
-round(avg(avg_price),2) price,
-round(avg(avg_salary),2) salary,
-FLOOR(avg(avg_salary)/avg(avg_price)) ratio 
-FROM t_vojtech_derner_project_sql_primary_final AS tvdpspf 
-WHERE salary_year IN (2006,2018)
-AND foodstuff LIKE 'Chléb konzumní kmínový'
-GROUP BY foodstuff, salary_year
-;
+WHERE 
+	salary_year IN (2006,2018)
+	AND foodstuff IN ('Mléko polotučné pasterované','Chléb konzumní kmínový')
+GROUP BY foodstuff, salary_year;
+
