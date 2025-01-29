0839314d58a121d73b749123728e0e1633f05ff7
Deri <vojtechderner@seznam.cz>
2025-01-30 00:24:38 +0100

    change view name

--- question_4.sql
+++ question_4.sql
@@ -1,7 +1,7 @@
 -- Q4: Existuje rok, ve kterém byl meziroční nárůst cen potravin 
 -- výrazně vyšší než růst mezd (větší než 10 %)?
 
-CREATE OR REPLACE VIEW v3 AS (
+CREATE OR REPLACE VIEW v3_vojtech_derner AS (
 SELECT 
 	salary_year,
 	round(((avg(avg_price) - lag(avg(avg_price)) OVER (PARTITION BY foodstuff ORDER BY salary_year))/lag(avg(avg_price)) OVER (PARTITION BY foodstuff ORDER BY salary_year)),4)*100 diff_per_price,
@@ -11,12 +11,12 @@ GROUP BY salary_year
 )
 
 SELECT 
-*,
-diff_per_price - diff_per_salary diff_diff
-FROM v3
+	*,
+	diff_per_price - diff_per_salary diff_diff
+FROM v3_vojtech_derner
 WHERE diff_per_price IS NOT NULL
 	OR diff_per_salary IS NOT NULL
--- HAVING abs(diff_diff) > 10
+	-- HAVING abs(diff_diff) > 10
 ;
 
 
