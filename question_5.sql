c3387189ef49f09e76c7d0de5c5e68ac16685d58
Deri <vojtechderner@seznam.cz>
2025-01-30 10:17:24 +0100

    Q5

---
+++ question_5.sql
@@ -0,0 +1,10 @@
+/* Q5: Má výška HDP vliv na změny ve mzdách a cenách potravin? 
+ 	Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to
+ 	na cenách potravin či mzdách ve stejném nebo násdujícím roce 
+ 	výraznějším růstem?
+*/
+
+SELECT * FROM t_vojtech_derner_project_sql_primary_final AS tvdpspf ;
+
+SELECT * FROM t_vojtech_derner_project_sql_secondary_final AS tvdpssf ;
+
