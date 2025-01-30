f8ecfb52830940f7e52fd2b45cc3dc220f5cbb69
Deri <vojtechderner@seznam.cz>
2025-01-30 20:14:27 +0100

    drop view, create new view and final select

--- question_5.sql
+++ question_5.sql
@@ -4,20 +4,7 @@
  	výraznějším růstem?
 */
 
-SELECT * FROM t_vojtech_derner_project_sql_primary_final AS tvdpspf ;
-
 CREATE OR REPLACE VIEW v4_vojtech_derner AS (
-SELECT
-`year`,
-GDP,
-round(((GDP - lag(GDP) OVER (PARTITION BY country ORDER BY year))/lag(GDP) OVER (PARTITION BY country ORDER BY year)),4)*100 diff_per_GDP
-FROM t_vojtech_derner_project_sql_secondary_final AS tvdpssf
-WHERE country = 'Czech Republic'
-);
-
-SELECT * FROM v4_vojtech_derner;
-
-WITH w2 AS (
 SELECT  
 	salary_year,
 	round(((ts.GDP - lag(ts.GDP) OVER (PARTITION BY ts.country ORDER BY ts.year))/lag(ts.GDP) OVER (PARTITION BY ts.country ORDER BY ts.year)),4)*100 diff_per_GDP,
@@ -26,19 +13,25 @@ SELECT
 	round(((avg(avg_salary) - lag(avg(avg_salary)) OVER (PARTITION BY industry ORDER BY salary_year))/lag(avg(avg_salary)) OVER (PARTITION BY industry ORDER BY salary_year)),4)*100 diff_per_salary,
 	round(((avg(avg_salary) - lag(avg(avg_salary),2) OVER (PARTITION BY industry ORDER BY salary_year))/lag(avg(avg_salary),2) OVER (PARTITION BY industry ORDER BY salary_year)),4)*100 diff_per_salary2
 FROM t_vojtech_derner_project_sql_primary_final AS tp
-JOIN t_vojtech_derner_project_sql_secondary_final AS ts ON tp.salary_year = ts.YEAR
+JOIN t_vojtech_derner_project_sql_secondary_final AS ts 
+	ON tp.salary_year = ts.YEAR
 WHERE ts.country = 'Czech Republic'
-GROUP BY tp.salary_year)
+GROUP BY tp.salary_year
+);
+
+SELECT * FROM v4_vojtech_derner;
+
 SELECT
-*,
-lead(diff_per_price2) OVER (ORDER BY salary_year)
-FROM w2;
-/*WHERE diff_per_price IS NOT NULL
+	salary_year,
+	diff_per_GDP,
+	diff_per_price,
+	lead(diff_per_price2) OVER (ORDER BY salary_year) diff_price_2y,
+	diff_per_salary,
+	lead(diff_per_salary2) OVER	(ORDER BY salary_year) diff_salary_2y
+FROM v4_vojtech_derner
+WHERE diff_per_price IS NOT NULL
 	OR diff_per_salary IS NOT NULL
-    OR diff_per_GDP IS NOT NULL 
-*/
--- WHERE diff_per_price IS NOT NULL
-	-- OR diff_per_salary IS NOT NULL
--- 	OR diff_per_GDP IS NOT NULL ;
+    OR diff_per_GDP IS NOT NULL;
+
 
 
